2925ded59c8b4378c007577e8abb1bf2e433b7cf
PierreRlld <84622002+PierreRlld@users.noreply.github.com>
2024-06-11 11:00:06 +0200

    Update ems_folder_pth.py

--- main/ems_folder_pth.py
+++ main/ems_folder_pth.py
@@ -1,11 +1,11 @@
 global today, base_path, clean_path, output_dir, cover_dir, git_pth, hk_chapt_name, converted
 
 #==== BASE_PATH used ====
-cover_dir = '/Volumes/222EXT/222Covers/'
-base_path = "/Volumes/222EXT/222Mangas_input/"
-clean_path = "/Volumes/222EXT/222Mangas_clean/"
-output_dir = '/Volumes/222EXT/222Mangas_output/'
-converted = '/Volumes/222EXT/Converted/'
+cover_dir = '/Volumes/222EXT/Scan_vault/222Covers/'
+base_path = "/Volumes/222EXT/Scan_vault/222Mangas_input/"
+clean_path = "/Volumes/222EXT/Scan_vault/222Mangas_clean/"
+output_dir = '/Volumes/222EXT/Scan_vault/222Mangas_output/'
+converted = '/Volumes/222EXT/Scan_vault/Converted/'
 git_pth = "https://github.com/PierreRlld/EMS"
 
 # Noms de chapt =/= de "Ch.X" ou "Chapter X"
